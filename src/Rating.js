61eaf4ede9c0cf57ff72cfe06b8b489621d473bd
Jeff Inman <jeffery.inman@hotmail.com>
2021-09-23 20:01:21 -0700

    got the star rating working

--- src/Rating.js
+++ src/Rating.js
@@ -4,21 +4,21 @@ import { FaStar } from "react-icons/fa";
 const Rating = () => {
   const [rating, setRating] = useState(null);
   const [hover, setHover] = useState(null);
-
+  
   return (
     <div>
       {[...Array(5)].map((star, i) => {
-        const ratingValue = i + 1;
-
-        return ( 
+        const ratingValue = i;
+        
+      return ( 
         <label>
           <input 
           type="radio" 
           name="rating" 
           value={ratingValue} 
-          onCLick={() => setRating(ratingValue)}/>
+          onClick={() => setRating(ratingValue)}/>
           <FaStar 
-          className="star" 
+          className="star"
           color={ratingValue < (hover ||rating) ? "#ffc107" : "#e4e5e9"} 
           size={25}
           onMouseEnter={() => setHover(ratingValue)}
