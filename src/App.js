b82d75cccd0a1d5bfd52795b6ec2d1281c8306ba
Jeff Inman <jeffery.inman@hotmail.com>
2021-09-25 13:48:51 -0700

    lab set up with updated readme informaiton

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
+import React from 'react';
 import './App.css';
 import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
 import Container from 'react-bootstrap/Container'
 
-function App() {
-  return (
-    <Container>
-      <Header/>
-      <Main/>
-      <Footer/>
-    </Container>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <Container>
+        <Header/>
+        <Main/>
+        <Footer/>
+      </Container>
+    );
+   }
 }
 
 export default App;
