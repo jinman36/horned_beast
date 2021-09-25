a2d13065bb896865c7380ccdea5c7582db6470fe
Jeff Inman <jeffery.inman@hotmail.com>
2021-09-25 14:01:11 -0700

    changed routing of json data directly to app.js from main

--- src/App.js
+++ src/App.js
@@ -4,14 +4,22 @@ import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
 import Container from 'react-bootstrap/Container'
+import dataJson from "./data.json"
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: dataJson
+    }
+  }
   render() {
     return (
       <Container>
         <Header/>
-        <Main/>
+        <Main
+        data={this.state.data}/>
         <Footer/>
       </Container>
     );
--- src/Main.js
+++ src/Main.js
@@ -1,18 +1,13 @@
 import React from "react";
 import HornedBeast from "./HornedBeast";
-import data from "./data.json"
 import CardGroup from "react-bootstrap/CardGroup";
 
 class Main extends React.Component {
   render() {
 
-    let hornedBeastArr = [];
-
-    data.forEach((beast, idx) => {
-      hornedBeastArr.push(
-        <HornedBeast key={idx} image_url={beast.image_url} title={beast.title}  description={beast.description} keyword={beast.keyword} horns={beast.horns}/>
-      )
-    })
+    let hornedBeastArr = this.props.data.map((beast, idx) => 
+      <HornedBeast key={idx} image_url={beast.image_url} title={beast.title}  description={beast.description} keyword={beast.keyword} horns={beast.horns}/>
+    )
     return (
       <>
       <CardGroup>
